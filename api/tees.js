0769cad8c4c68938d210ad5ffe0abb4e73441e48
Lorenzo Vannucci <info@vannucci-dev.com>
2021-05-13 13:07:38 +0100

    change temporary DB schema to facilitate CRUD operations - Add GET_POST_PUT for api/tees

--- api/tees.js
+++ api/tees.js
@@ -4,11 +4,34 @@ import teesDB from "../tees-DB.js";
 const tees = express.Router();
 
 tees.get("/", (req, res) => {
-  res.send("All the tees");
+  res.json(teesDB);
 });
 
 tees.get("/:id", (req, res) => {
-  res.json(teesDB.find((tee) => tee.id === +req.params.id));
+  if (teesDB[`tee${req.params.id}`] != null) {
+    res.status(200).send(teesDB[`tee${req.params.id}`]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+tees.post("/", (req, res) => {
+  const newTee = req.body;
+
+  teesDB[`tee${newTee.id}`] = newTee;
+  res.status(201).send(teesDB);
+});
+
+tees.put("/:id", (req, res) => {
+  var id = parseInt(req.params.id);
+  const updatedTee = req.body;
+
+  if (teesDB[`tee${id}`] != null) {
+    teesDB[`tee${id}`] = updatedTee;
+    res.status(200).send(teesDB[`tee${id}`]);
+  } else {
+    res.status(400).send();
+  }
 });
 
 export default tees;
--- tees-DB.js
+++ tees-DB.js
@@ -1,14 +1,14 @@
-const tees = [
-  {
+const tees = {
+  tee1: {
     id: 1,
     name: "Cat-Tee",
     available_sizes: ["s", "m", "l"],
   },
-  {
+  tee2: {
     id: 2,
     name: "Dog-Tee",
     available_sizes: ["l", "xl"],
   },
-];
+};
 
 export default tees;
