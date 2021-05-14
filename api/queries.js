caedb2a83d6842255ed134cdd50c7af48b88bbb8
Lorenzo Vannucci <info@vannucci-dev.com>
2021-05-14 18:30:14 +0100

    add GET/:id, POST, PUT and DELETE methods to complete CRUD

--- api/queries.js
+++ api/queries.js
@@ -12,6 +12,7 @@ const pool = new Pool({
   port: 5432,
 });
 
+// GET api/tees
 const getAllTees = (req, res) => {
   pool.query("SELECT * FROM tees", (err, results) => {
     if (err) {
@@ -20,7 +21,62 @@ const getAllTees = (req, res) => {
     res.status(200).json(results.rows);
   });
 };
+// GET api/tees/:id
+const getSingleTeeById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query("SELECT * FROM tees WHERE id = $1", [id], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+//POST api/tees
+const createNewTee = (req, res) => {
+  const { name, available_sizes, available_colors } = req.body;
+
+  pool.query(
+    "INSERT INTO tees (name, available_sizes, available_colors) VALUES ($1, $2, $3)",
+    [name, available_sizes, available_colors],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      res.status(201).json();
+    }
+  );
+};
+//PUT api/tees/:id
+const updateTee = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, available_sizes, available_colors } = req.body;
+  pool.query(
+    "UPDATE tees SET name = $2, available_sizes = $3, available_colors = $4 WHERE id = $1",
+    [id, name, available_sizes, available_colors],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json();
+    }
+  );
+};
+//DELETE api/tees/:id
+const deleteTee = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query("DELETE FROM tees WHERE id = $1", [id], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json();
+  });
+};
 
 module.exports = {
   getAllTees,
+  getSingleTeeById,
+  createNewTee,
+  updateTee,
+  deleteTee,
 };
--- api/tees.js
+++ api/tees.js
@@ -4,34 +4,9 @@ const db = require("./queries.js");
 const tees = express.Router();
 
 tees.get("/", db.getAllTees);
-
-/*
-tees.get("/:id", (req, res) => {
-  if (teesDB[`tee${req.params.id}`] != null) {
-    res.status(200).send(teesDB[`tee${req.params.id}`]);
-  } else {
-    res.status(404).send();
-  }
-});
-
-tees.post("/", (req, res) => {
-  const newTee = req.body;
-
-  teesDB[`tee${newTee.id}`] = newTee;
-  res.status(201).send(teesDB);
-});
-
-tees.put("/:id", (req, res) => {
-  var id = parseInt(req.params.id);
-  const updatedTee = req.body;
-
-  if (teesDB[`tee${id}`] != null) {
-    teesDB[`tee${id}`] = updatedTee;
-    res.status(200).send(teesDB[`tee${id}`]);
-  } else {
-    res.status(400).send();
-  }
-});
-*/
+tees.get("/:id", db.getSingleTeeById);
+tees.post("/", db.createNewTee);
+tees.put("/:id", db.updateTee);
+tees.delete("/:id", db.deleteTee);
 
 module.exports = tees;
